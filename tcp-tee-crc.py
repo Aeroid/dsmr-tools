5b0136b8ac068b0243388b0f210619857b4c722e
Gunnar Klauberg <Gunnar@Klauberg.de>
2024-02-08 23:33:35 +0100

    rewrite of reformat_evcc()
    avoid duplication of 2.7.0

--- tcp-tee-crc.py
+++ tcp-tee-crc.py
@@ -38,38 +38,58 @@ def add_crc(packet):
     crc = crc16(checksum_contents.group(0))
     
     return "{}{:04X}\r\n".format(checksum_contents.group(0) , crc)
-    
-def toKW(v):
-    """ turn 000847.48*W into 00000.84748*kW """
-    (val, unit) = v.split("*")
-    val = float(val)
-    if (unit == "W"):
-        val = val/1000
-        unit = "kW"
-        
-    return "{:011.5f}*{}".format(val,unit);
 
 def reformat_evcc(packet):
-    """ turn 
-        1-0:1.7.0*255(-000847.48*W)
+    """ for evcc turn 
+        1-0:1.7.0*255(-000333.00*W)
+        1-0:2.7.0*255(01000.00000*W)
       into  
         1-0:1.7.0(00000.00000*kW)
-        1-0:2.7.0(00000.84748*kW)
+        1-0:2.7.0(00001.33300*kW)
     """
-    consumption_re = re.search(r'(1\-0\:1\.7\.0\*255)\((.+)\)', packet)
-    cval = consumption_re.group(2)
-    pval = "0*W"
-    if (cval[:1] == "-"):
-        pval = cval[1:]
-        cval = "0*W";
-    production = "1-0:2.7.0*255("+toKW(pval)+")"
-    consumption = "1-0:1.7.0*255("+toKW(cval)+")"
+    packet = re.sub(r'(\/[.\w]* [.\w]*)', r'\1 evcc', packet)
+    # reead production
+    production_re = re.search(r'(1\-0\:2\.7\.0\*255)\((.+)\*(.+)\)', packet)
+    if production_re:
+        p = [float(production_re.group(2)), production_re.group(3)]
+        if p[1] == "MW":
+            p[0] = p[0]*1000.0
+            p[1] = "kW"
+        if p[1] == "W":
+            p[0] = p[0]/1000.0
+            p[1] = "kW"
+    else:
+        p = [0.0,"kW"]
+    
+    # remove production
+    packet = re.sub(r'1\-0\:2\.7\.0.*[\n\r]*', '', packet)
+
+    # read consumption
+    consumption_re = re.search(r'(1\-0\:1\.7\.0\*255)\((.+)\*(.+)\)', packet)
+    c = [float(consumption_re.group(2)),consumption_re.group(3)]
+    if c[1] == "MW":
+        c[0] = c[0]*1000.0
+        c[1] = "kW"
+    if c[1] == "W":
+        c[0] = c[0]/1000.0
+        c[1] = "kW"
+
+    # turn negative consumption into positive production
+    if (c[0] < 0):
+        p[0] = p[0] - c[0] 
+        c = [0.0, "kW"]
+    # p[0] = p[0] + 0.4711
+    # W -> kW
+    production = "1-0:2.7.0*255({:011.5f}*{})".format(p[0],p[1])
+    consumption = "1-0:1.7.0*255({:011.5f}*{})".format(c[0],c[1])
     insert = consumption +"\r\n"+ production
     packet = re.sub(r'(1\-0\:1\.7\.0\*255)\((.+)\)', insert, packet)
+    
+    # remove *255
     packet = re.sub(r'(\d)(\*255)\(', r'\1(', packet)
     
     return packet;
-    
+
 def strhex(s):
     return ":".join("{:02x}".format(ord(c)) for c in s)
 
