b39bfdfa463ac646a169355a8b0ef2d56f722999
Gunnar Klauberg <Gunnar@Klauberg.de>
2024-02-09 18:47:02 +0100

    new arg -w
    default is to normalize to kW (evcc DSMR parser)
    but be now be switched to normalize to W (Watts) to support MQTT wrapping

--- tcp-tee-crc.py
+++ tcp-tee-crc.py
@@ -38,7 +38,33 @@ def add_crc(packet):
     crc = crc16(checksum_contents.group(0))
     
     return "{}{:04X}\r\n".format(checksum_contents.group(0) , crc)
+    
+def recalc(v, target_unit):
+    if not v:
+        return
+    divisor = 1.0
+    if target_unit == "MW":
+        divisor = 1000.0 * 1000.0
+    elif target_unit == "kW":
+        divisor = 1000.0
+    multiplier = 1.0
+    if v[1] == "MW":
+        multiplier = 1000.0 * 1000.0
+    elif v[1] == "kW":
+        multiplier = 1000.0
+    v[0] = v[0] * multiplier / divisor
+    v[1] = target_unit
+    return v
 
+# International generalized additions
+# ELECTRICITY_IMPORTED_TOTAL    = :1.8.0    # Total imported energy register (P+)
+# ELECTRICITY_EXPORTED_TOTAL    = :2.8.0    # Total exported energy register (P-)
+# CURRENT_ELECTRICITY_USAGE     = :1.7.0    # consumption
+# CURRENT_ELECTRICITY_DELIVERY  = :2.7.0    # production
+# Q3D_EQUIPMENT_IDENTIFIER      = :0.0.0    # Logical device name
+# Q3D_EQUIPMENT_SERIALNUMBER    = :96.1.255 # Device Serialnumber
+
+target_unit = "kW"
 def reformat_evcc(packet):
     """ for evcc turn 
         1-0:1.7.0*255(-000333.00*W)
@@ -51,34 +77,22 @@ def reformat_evcc(packet):
     # reead production
     production_re = re.search(r'(1\-0\:2\.7\.0\*255)\((.+)\*(.+)\)', packet)
     if production_re:
-        p = [float(production_re.group(2)), production_re.group(3)]
-        if p[1] == "MW":
-            p[0] = p[0]*1000.0
-            p[1] = "kW"
-        if p[1] == "W":
-            p[0] = p[0]/1000.0
-            p[1] = "kW"
+        p = recalc([float(production_re.group(2)), production_re.group(3)],target_unit)
     else:
-        p = [0.0,"kW"]
+        p = [0.0,target_unit]
     
     # remove production
     packet = re.sub(r'1\-0\:2\.7\.0.*[\n\r]*', '', packet)
 
     # read consumption
     consumption_re = re.search(r'(1\-0\:1\.7\.0\*255)\((.+)\*(.+)\)', packet)
-    c = [float(consumption_re.group(2)),consumption_re.group(3)]
-    if c[1] == "MW":
-        c[0] = c[0]*1000.0
-        c[1] = "kW"
-    if c[1] == "W":
-        c[0] = c[0]/1000.0
-        c[1] = "kW"
+    c = recalc([float(consumption_re.group(2)),consumption_re.group(3)],target_unit)
 
     # turn negative consumption into positive production
     if (c[0] < 0):
         p[0] = p[0] - c[0] 
-        c = [0.0, "kW"]
-    # p[0] = p[0] + 0.4711
+        c = [0.0, target_unit]
+    # p[0] = p[0] + 4711
     # W -> kW
     production = "1-0:2.7.0*255({:011.5f}*{})".format(p[0],p[1])
     consumption = "1-0:1.7.0*255({:011.5f}*{})".format(c[0],c[1])
@@ -152,6 +166,7 @@ if __name__ == "__main__":
     parser.add_argument('remoteport', help='TCP port to read from')
     parser.add_argument('serverport', help='TCP port to serve from')
     parser.add_argument('-c', '--crc', action='store_true', help='Enable CRC mode and removes any junk')
+    parser.add_argument('-w', '--watt', action='store_true', help='normalize values on Watts')
     parser.add_argument('-e', '--evcc', action='store_true', help='Enable EVCC mode (https://github.com/evcc-io/evcc/issues/11772)')
     args = parser.parse_args()
     server_host = '0.0.0.0'
@@ -159,6 +174,8 @@ if __name__ == "__main__":
     generate_evcc = args.evcc
     if (generate_evcc):
         generate_crc = True
+    if (args.watt):
+        target_unit = "W"
 
     remote_host = args.remotehost
     remote_port = int(args.remoteport)
